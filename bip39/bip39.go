e562ff9f13ca5aeffde252d888ace1bb841e7f78
pilanias38 <pilanias38@gmail.com>
2024-03-08 08:59:57 +0530

    complted

--- bip39/bip39.go
+++ bip39/bip39.go
@@ -12,11 +12,35 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"math/big"
+
+	_ "embed"
+	"hash/crc32"
 	"strings"
 
 	"github.com/pkg/errors"
+
 	"golang.org/x/crypto/pbkdf2"
 )
+
+//go:embed wordlist.txt
+var words string
+
+// Words a slice of mnemonic words taken from the bip39 specification
+// https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt
+var (
+	Words = strings.Split(strings.TrimSpace(words), "\n")
+)
+
+func init() {
+	// Ensure word list is correct
+	// $ wget https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt
+	// $ crc32 english.txt
+	// OUTPUT: c1dbd296
+	checksum := crc32.ChecksumIEEE([]byte(words))
+	if checksum != 0xc1dbd296 {
+		panic(errors.Errorf("wordlist checksum mismatch: expected %x, got %x", 0xc1dbd296, checksum))
+	}
+}
 
 var (
 	one = big.NewInt(1)
